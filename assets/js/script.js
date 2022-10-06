bb8bc087bb94c044d50be5ab2a87d8ed0348e4e2
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-10-06 08:19:41 +0000

    add cards to cardDeck array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,25 @@ document.addEventListener('DOMContentLoaded'), () => {
 
 
     const cardDeck = [
+        card: 'card1',
+        
+        card: 'card2',
+        
+        card: 'card3',
+
+        card: 'card4',
+        
+        card: 'card5',
+        
+        card: 'card6',
+
+        card: 'card7',
+        
+        card: 'card8',
 
     ]
 
     function callDeck() {
-
     }
 
     function checkForMatch() {
