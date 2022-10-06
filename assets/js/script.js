42e499d2e5992217588b91c388a2d89258cbc595
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-10-06 08:43:33 +0000

    add back to for in loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,43 +3,42 @@ document.addEventListener('DOMContentLoaded'), () => {
 
     const cardDeck = [
         {
-            card: 'card1'
-            img: './memory_game/assets/images/alarm_clock.png'
+            card: 'card1',
+            img: '.assets/images/alarm_clock.png',
         },
         
         {
-            card: 'card2'
-            img: './memory_game/assets/images/camera.png'
+            card: 'card2',
+            img: '.assets/images/camera.png',
+
+        {
+            card: 'card3',
+            img: '.assets/images/envelope.png',
         },
 
         {
-            card: 'card3'
-            img: './memory_game/assets/images/envelope.png'
+            card: 'card4',
+            img: '.assets/images/smiley.png',
         },
 
         {
-            card: 'card4'
-            img: './memory_game/assets/images/smiley.png'
+            card: 'card5',
+            img: '.assets/images/soup.png',
         },
 
         {
-            card: 'card5'
-            img: './memory_game/assets/images/soup.png'
+            card: 'card6',
+            img: '.assets/images/timer.png',
         },
 
         {
-            card: 'card6'
-            img: './memory_game/assets/images/timer.png'
+            card: 'card7',
+            img: '.assets/images/water_drop.png',
         },
 
         {
-            card: 'card7'
-            img: './memory_game/assets/images/water_drop.png'
-        },
-
-        {
-            card: 'card8'
-            img: './memory_game/assets/images/yoda.png'
+            card: 'card8',
+            img: '.assets/images/yoda.png',
         }
 
     ]
@@ -47,9 +46,9 @@ document.addEventListener('DOMContentLoaded'), () => {
     const grid = document.getElementsByClassName('game_field');
 
     function callDeck() {
-        for (let i = 0; i <cardDeck.length; i++ ) {
+        for (let card in cardDeck) {
             var gameCard = document.createElement('img');
-            gameCard.setAttribute('src', '/assets/images.png');
+            gameCard.setAttribute('src', '/assets/images');
             gameCard.setAttribute('card_id', card);
             grid.appendChild(gameCard); 
         }
