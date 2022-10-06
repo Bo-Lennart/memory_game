836a794c340bdd2ad1cf5fea6b234c46d954f948
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-10-06 09:08:43 +0000

    start over coding the JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,56 @@
 document.addEventListener('DOMContentLoaded'), () => {
 
-
+// Memory game deck options
     const cardDeck = [
         {
             card: 'card1',
-            img: '.assets/images/alarm_clock.png',
+            img: 'assets/images/alarm_clock.png'
         },
         
         {
             card: 'card2',
-            img: '.assets/images/camera.png',
+            img: 'assets/images/camera.png'
 
         },
         
         {
             card: 'card3',
-            img: '.assets/images/envelope.png',
+            img: 'assets/images/envelope.png'
         },
 
         {
             card: 'card4',
-            img: '.assets/images/smiley.png',
+            img: 'assets/images/smiley.png'
         },
 
         {
             card: 'card5',
-            img: '.assets/images/soup.png',
+            img: 'assets/images/soup.png'
         },
 
         {
             card: 'card6',
-            img: '.assets/images/timer.png',
+            img: 'assets/images/timer.png'
         },
 
         {
             card: 'card7',
-            img: '.assets/images/water_drop.png',
+            img: 'assets/images/water_drop.png'
         },
 
         {
             card: 'card8',
-            img: '.assets/images/yoda.png',
+            img: 'assets/images/yoda.png'
         }
 
     ]
-
+    
     const grid = document.getElementsByClassName('game_field');
 
-    function callDeck() {
-        for (let card in cardDeck) {
-            var gameCard = document.createElement('img');
-            gameCard.setAttribute('src', '/assets/images');
-            gameCard.setAttribute('card_id', card);
-            grid.appendChild(gameCard); 
-        }
-    }
+    
 
+    function callDeck() {
+    }
 
     function checkForMatch() {
 
@@ -65,6 +60,8 @@ document.addEventListener('DOMContentLoaded'), () => {
 
     }
 
-callDeck()
+
+
+    callDeck()
 
 }
