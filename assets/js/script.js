71da6987ec00cc0a4f89c370ea4abf3cf0e88b9e
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-10-06 13:15:00 +0000

    copy card arrayws so there exists two of each

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,16 @@ document.addEventListener('DOMContentLoaded', () => {
             card: 'card1',
             img: 'assets/images/alarm_clock.png'
         },
+        {
+            card: 'card1',
+            img: 'assets/images/alarm_clock.png'
+        },
         
+        {
+            card: 'card2',
+            img: 'assets/images/camera.png'
+
+        },
         {
             card: 'card2',
             img: 'assets/images/camera.png'
@@ -17,7 +26,15 @@ document.addEventListener('DOMContentLoaded', () => {
             card: 'card3',
             img: 'assets/images/envelope.png'
         },
+        {
+            card: 'card3',
+            img: 'assets/images/envelope.png'
+        },
 
+        {
+            card: 'card4',
+            img: 'assets/images/smiley.png'
+        },
         {
             card: 'card4',
             img: 'assets/images/smiley.png'
@@ -27,7 +44,15 @@ document.addEventListener('DOMContentLoaded', () => {
             card: 'card5',
             img: 'assets/images/soup.png'
         },
+        {
+            card: 'card5',
+            img: 'assets/images/soup.png'
+        },
 
+        {
+            card: 'card6',
+            img: 'assets/images/timer.png'
+        },
         {
             card: 'card6',
             img: 'assets/images/timer.png'
@@ -37,7 +62,15 @@ document.addEventListener('DOMContentLoaded', () => {
             card: 'card7',
             img: 'assets/images/water_drop.png'
         },
+        {
+            card: 'card7',
+            img: 'assets/images/water_drop.png'
+        },
 
+        {
+            card: 'card8',
+            img: 'assets/images/yoda.png'
+        }
         {
             card: 'card8',
             img: 'assets/images/yoda.png'
@@ -46,7 +79,11 @@ document.addEventListener('DOMContentLoaded', () => {
     ]
     
     const gameBoard = document.getElementById('game_body')
+    
 
+    /**
+     * Create game board out of cardDeck array
+     */
     function callDeck() {
         for (let img in cardDeck) {
             var card = document.createElement('img')
@@ -57,7 +94,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function checkForMatch() {
-
+        
     }
 
     function turnCard() {
