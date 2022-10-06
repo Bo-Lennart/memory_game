35d3639ec9092cd09fc654fe63db15c7378776a8
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-10-06 08:28:55 +0000

    add images to array

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,21 +2,45 @@ document.addEventListener('DOMContentLoaded'), () => {
 
 
     const cardDeck = [
-        card: 'card1',
+        {
+            card: 'card1'
+            img: './memory_game/assets/images/alarm_clock.png'
+        },
         
-        card: 'card2',
-        
-        card: 'card3',
+        {
+            card: 'card2'
+            img: './memory_game/assets/images/camera.png'
+        },
 
-        card: 'card4',
-        
-        card: 'card5',
-        
-        card: 'card6',
+        {
+            card: 'card3'
+            img: './memory_game/assets/images/envelope.png'
+        },
 
-        card: 'card7',
-        
-        card: 'card8',
+        {
+            card: 'card4'
+            img: './memory_game/assets/images/smiley.png'
+        },
+
+        {
+            card: 'card5'
+            img: './memory_game/assets/images/soup.png'
+        },
+
+        {
+            card: 'card6'
+            img: './memory_game/assets/images/timer.png'
+        },
+
+        {
+            card: 'card7'
+            img: './memory_game/assets/images/water_drop.png'
+        },
+
+        {
+            card: 'card8'
+            img: './memory_game/assets/images/yoda.png'
+        },
 
     ]
 
