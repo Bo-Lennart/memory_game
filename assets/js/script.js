dfd84fbb1fc31c085a7a3429a642fa7370c168e7
Bo-Lennart Heitmann <bo.heitmann@gmail.com>
2022-10-10 10:48:37 +0000

    add font style to lose and win statement

--- assets/js/script.js
+++ assets/js/script.js
@@ -512,14 +512,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
             if (attempts === 20 || failScore + attempts === 20) {
                 body = document.getElementById('call_for_reload');
-                body.setAttribute('style', 'background-color: #9e2a2b;');
+                body.setAttribute('style', 'color: #9e2a2b;');
                 var loser = document.createElement('h2');
                 body.appendChild(loser);
                 loser.innerHTML = "You LOST!"
 
             } else {
                 body = document.getElementById('call_for_reload');
-                body.setAttribute('style', 'background-color: #a3b18a;');
+                body.setAttribute('style', 'color: rgb(145, 235, 137);');
                 var loser = document.createElement('h2');
                 body.appendChild(loser);
                 loser.innerHTML = "You WON!! CONGRATS!"
